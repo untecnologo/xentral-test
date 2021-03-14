aa664a0449573168b7525057d7bf500aec60542b
Rafael Aguilera <r.aguilera@caala.de>
2021-03-15 01:41:52 +0200

    User Registration working PDO

--- php/classes/db_connections.class.php
+++ php/classes/db_connections.class.php
@@ -8,9 +8,43 @@
     protected function connect(){
         
         $dsn = 'mysql:host='.$this->host.';dbname='.$this->dbName;
-        $pdo = new PDO($dsn, $this->user, $this->pwd);
-        $pdo->setAttribute(PDO::ATTR_DEFAUL_FETCH_MODE, PDO::FETCH_ASSOC);
+        try {
 
+            $pdo = new PDO($dsn, $this->user, $this->pwd);
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+           // echo "Connected successfully";
+        } catch(PDOException $e) {
+           // echo "Connection failed: " . $e->getMessage();
+        } 
+        
         return $pdo;
     }
+
+    protected function register_user($user_parameter, $psw_parameter){
+        $pdo_updated = $this->connect();
+        $stmt = $pdo_updated->prepare("SELECT email FROM users WHERE email = :email_user");
+        $stmt->bindParam(':email_user', $user_parameter);
+        $stmt->execute();
+
+        if ($stmt->rowCount()>0){
+            
+            $this->drop_connect($pdo_updated, $stmt);
+            return false;
+        } else {
+            $stmt = $pdo_updated->prepare("INSERT INTO users (email, password) VALUES (:email_user, :password_user)");
+            $stmt->bindParam('email_user', $user_parameter);
+            $stmt->bindParam('password_user', $psw_parameter);
+            $stmt->execute();
+
+            $this->drop_connect($pdo_updated, $stmt);
+            return true;
+        }
+    }
+
+    protected function drop_connect($pdo, $stmt){
+        $pdo = null;
+        $stmt =null;
+    }
+
+
 }
--- php/classes/user_options.class.php
+++ php/classes/user_options.class.php
@@ -1,23 +1,24 @@
 <?php   
-class user_act extends db_conn_secure {
+
+include 'db_connections.class.php';
+
+class user_options extends db_connections {
 
     public $user;
     public $password;
-
-    function __construct ($user, $password){
-        $this->user = $user;
-        $this->password = $password;
-    }
-
        
-    function create_user(){
-    
+    public function create_user($email,$pwd){
+        $this->user = $email;
+        $this->password = $pwd;
+        $connection = new db_connections();
+        $resultq = $connection->register_user($this->user, $this->password);
+
+        if ($resultq === false){
+            return false;
+        }else{
+            return true;
+        }
     }
-
-    function __destruct() {
-        echo "The fruit is {$this->name}.";
-      }
 }
-
 
 ?>
